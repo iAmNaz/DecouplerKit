20c0361c1ce1e6bc18a6e0f20ad9ed3800e249a0
Naz <naz.workaddress@gmail.com>
2018-06-08 17:49:52 +0800

    Remove commented code

--- Example/DecouplerKit/ViewControllers/HomeTableViewController.swift
+++ Example/DecouplerKit/ViewControllers/HomeTableViewController.swift
@@ -94,29 +94,4 @@ class HomeTableViewController: UITableViewController {
             let _  = response
         }
     }
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
