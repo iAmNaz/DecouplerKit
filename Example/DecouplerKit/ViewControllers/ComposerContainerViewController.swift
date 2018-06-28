d28f4f1f4dbb46e38bfc50ae7c2a23cd41eb7490
Naz <naz.workaddress@gmail.com>
2018-06-28 20:13:01 +0800

    Minor clean up

--- Example/DecouplerKit/ViewControllers/ComposerContainerViewController.swift
+++ Example/DecouplerKit/ViewControllers/ComposerContainerViewController.swift
@@ -27,8 +27,7 @@ class ComposerContainerViewController: UIViewController {
     }
     
     func dismissMe() {
-        dismiss(animated: true) {
-        }
+        dismiss(animated: true) {}
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -41,24 +40,10 @@ class ComposerContainerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
